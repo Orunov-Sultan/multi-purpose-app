150ee1c5fbae0439c4a2e4d83184ea00faeef41b
Sultan-home <sultat.orunov@mail.ru>
2023-01-16 19:28:56 +0500

    install admin panel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('{view}', \App\Http\Controllers\ApplicationController::class)->where('view', '.*');
